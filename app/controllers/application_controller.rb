88dd1d848950e9dfc7188a2f19f9250c7d5e09cd
OrestiArsi <orestiarsi@gmail.com>
2017-10-01 00:22:35 -0400

    added a json response for applicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+ rescue_from ActiveRecord::RecordNotFound do |exception|
+  respond_to :json
+  render json: { error: exception.message }, status: :not_found
+ end
+
 end
